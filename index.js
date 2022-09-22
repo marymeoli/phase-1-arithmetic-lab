c7dac1bb74afd8282cf6299cf21809e5c09f3ed0
Moringa School <marymeoli77@gmail.com>
2022-09-22 12:56:12 +0300

    submit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 =62
+const num2 =1
+const multiply =num1 * num2
+
+const random= num1;
+Math.random(num1)
+
+const num3 =28;
+const num4 =6;
+const mod = num3%num4
+
+let max =20
+Math.max(1,2,3,4,20)
